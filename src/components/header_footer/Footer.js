ba6a1e58998294b475eb20ba75287915570d8f25
Aditya Mankare <adityamankare@gmail.com>
2019-08-27 10:34:13 -0500

    Add files via upload

---
+++ src/components/header_footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Footer = () => {
+    return (
+        <footer className="bck_black">
+            <Fade delay={500}>
+                <div className="font_righteous footer_logo_venue">See you there!</div>
+                <div className="footer_copyright">
+                    
+                </div>
+            </Fade>
+        </footer>
+    );
+};
+
+export default Footer;
