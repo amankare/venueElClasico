f2578a544016583e5bf8328dbc52de0c472cf54d
Aditya Mankare <adityamankare@gmail.com>
2019-08-27 10:37:33 -0500

    Add files via upload

---
+++ build/precache-manifest.99b01450ba2ed2429e2dec0a659ab97e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388b7396508c4f380f5c4878476e0800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b1cf015a069d08559df",
+    "url": "/static/css/main.e65d2b22.chunk.css"
+  },
+  {
+    "revision": "c57b8bbbfc10d7429ac6",
+    "url": "/static/js/2.77d2b66a.chunk.js"
+  },
+  {
+    "revision": "1b1cf015a069d08559df",
+    "url": "/static/js/main.c649ce82.chunk.js"
+  },
+  {
+    "revision": "bbf2b61067fe63e36be8",
+    "url": "/static/js/runtime~main.48818f58.js"
+  },
+  {
+    "revision": "f24b5e61fbff68f16588b689aa405b56",
+    "url": "/static/media/slide_one.f24b5e61.jpg"
+  },
+  {
+    "revision": "449c28c9fa218e980837e0acbefa8992",
+    "url": "/static/media/slide_three.449c28c9.jpg"
+  },
+  {
+    "revision": "b5aa7dc62aad36a54367ecdb28260e27",
+    "url": "/static/media/slide_two.b5aa7dc6.jpg"
+  }
+]);
